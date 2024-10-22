2bfd080cb763f2e52f8af5bf7f83094af6995892
Vinícius Aquino <vinicius.aquino@estudante.ufcg.edu.br>
2024-10-22 02:05:24 -0300

    Add files via upload

---
+++ MakeTriangule/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace MakeTriangule {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.Write("Stairs height: ");
+            int height = int.Parse(Console.ReadLine());
+            string stair = "#";
+            string space = " ";
+            for (int i = 1; i <= height; i++) {
+                string spaces = String.Concat(Enumerable.Repeat(space, height - i));
+                string stairs = String.Concat(Enumerable.Repeat(stair, i));
+                Console.WriteLine(spaces + stairs);
+            }
+        }
+    }
+}
